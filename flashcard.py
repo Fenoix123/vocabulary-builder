40fb44da3075104243e35fc9eaf79f722695b13b
Fenoix123 <femiadegbuji@gmail.com>
2026-09-02 21:02:47 +0100

    Small tweak

--- flashcard.py
+++ flashcard.py
@@ -3,22 +3,23 @@ class Flashcard:
     def __init__(self):
         self.storage = VocabularyStorage() # It creates your storage object
         self.words = self.storage.load_words() # Loads the words already saved in vocabulary.json
-        print(self.words)
+    
 
     def start(self):
 
         if not self.words:
-            print(str("No saved words available."))
-        for word, details in self.words.items():
-            print(f"\nWord: {word}") # it displays each word one at a time
-            answer = input("show definition? (y/n or q to quit): ") #this asks the user to choose what to dor each word
+            print("No saved words available.")
+            return 
 
-            if answer.lower() in ["q", "quit"]: # makes q , quit all work
+        for word, details in self.words.items():
+            print(f"\nWord: {word}") # Displays each word one at a time
+            answer = input("show definition? (y/n or q to quit): ") # Lets the user reveal the definition or quit
+
+            if answer.lower() in ["q", "quit"]: # exits the flashcards
                 break
 
             if answer.lower() in ["y", "yes"]: # y/yes shows the definition
-                # n/no does not show the definition but moves to the next word
-                # exits the flashcards
+                
                 print(f"Definition: {details['definition']}")
 
 if __name__=="__main__":
